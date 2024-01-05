5fcd46f158bec8c27b804c451397b19983528274
Joseph Odunsi <odunsiolakunbi@gmail.com>
2024-01-05 12:46:10 +0000

    fix: Delete unnecessary files and update package.json author

--- build/modules/root/root.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QuerystringSchema = void 0;
-const zod_1 = require("zod");
-exports.QuerystringSchema = zod_1.z.object({
-    sub_id: zod_1.z.string().default(""),
-    reg: zod_1.z.string().default(""),
-    aReg: zod_1.z.string().default(""),
-});
